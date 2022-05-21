ff4cb0a220f9a9ccfc68693c7392eff24436c3f3
Maxim <vedernikovmaxim@rambler.ru>
2022-05-21 21:49:41 +0300

    Решена третья задача

--- Задача_1_список_из_N_членов/.py
+++ Задача_1_список_из_N_членов/.py
@@ -1,17 +1,17 @@
-#Сформировать список из N членов последовательности.
-#Для N = 5: 1, -3, 9, -27, 81 и т.д.
+# Сформировать список из N членов последовательности.
+# Для N = 5: 1, -3, 9, -27, 81 и т.д.
 
 def GeometricSequence(number):
     if number == 0:
         print("Последовательность не задана")
     else:
-        count =1
-        list =[count]
+        count = 1
+        list = [count]
         for i in range(number-1):
-            count*=(-3)
+            count *= (-3)
             list.append(count)
         print(f"Последовательность чисел: {list}")
-    
+
 
 print("Задайте число >0")
 numberUser = int(input())
